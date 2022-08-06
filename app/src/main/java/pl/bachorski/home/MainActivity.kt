762912413cbdbc1a8f542f6773c55bc34e2c1d9b
Marek Bachorski <m.bachorski@gmail.com>
2022-08-06 12:32:19 +0200

    initial commit

---
+++ app/src/main/java/pl/bachorski/home/MainActivity.kt
@@ -0,0 +1,18 @@
+package pl.bachorski.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.bachorski.home.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/pl/bachorski/home/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package pl.bachorski.home.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
